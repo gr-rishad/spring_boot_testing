46a488664afa3310f6214ac550c5281d4ff92619
gr-rishad <gr.rishad@gmail.com>
2023-01-29 01:00:59 +0600

    service layer save object exception

--- spring boot 3 with JUnit and mockito/spring-boot-testing/src/test/java/com/javaguides/springboot/service/EmployeeServiceTests.java
+++ spring boot 3 with JUnit and mockito/spring-boot-testing/src/test/java/com/javaguides/springboot/service/EmployeeServiceTests.java
@@ -1,30 +1,88 @@
 package com.javaguides.springboot.service;
 
+import com.javaguides.springboot.exception.ResourceNotFundException;
+import com.javaguides.springboot.model.Employee;
 import com.javaguides.springboot.repository.EmployeeRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
 
+
+    /*
+    || -->  dependency injection using mock() method --> Mockito.mock()
     private EmployeeRepository employeeRepository;
     private EmployeeService employeeService;
-
     @BeforeEach
     public void setup() {
         employeeRepository = Mockito.mock(EmployeeRepository.class);
         employeeService = new EmployeeServiceImpl(employeeRepository);
     }
+ */
+
+    // dependency inject using Annotation
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+    private Employee employee;
+
+    @BeforeEach
+    public void setup() {
+        employee = Employee.builder()
+                .id(1L)
+                .firstName("GOlam Rabbani")
+                .lastName("Rishad")
+                .email("gr.rishad@gmail.com")
+                .build();
+    }
 
 
     // JUnit test for saveEmployee method
+    @DisplayName("JUnit test for savedEmployee method")
     @Test
     public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
         // given ->  precondition or setup
-        
+        // what saveEmployee() method internally return ---> employeeRepository.findByEmail() & employeeRepository.save()
+        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.empty());
+        BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
 
         // when -> action or the behavior that we are testing
+        Employee savedEmployee = employeeService.saveEmployee(employee);
 
         // then -> verify the output
+        Assertions.assertThat(savedEmployee).isNotNull();
+    }
+
+    // JUnit test for saveEmployee method
+    @DisplayName("JUnit test for savedEmployee method which throws exception")
+    @Test
+    public void givenExistingEmail_whenSaveEmployee_thenThrowsException() {
+        // given ->  precondition or setup
+        // what saveEmployee() method internally return ---> employeeRepository.findByEmail() & employeeRepository.save()
+        BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
+                .willReturn(Optional.of(employee));
+        //   BDDMockito.given(employeeRepository.save(employee)).willReturn(employee);
+
+        // when -> action or the behavior that we are testing
+        org.junit.jupiter.api.Assertions.assertThrows(ResourceNotFundException.class, () -> {
+            employeeService.saveEmployee(employee);
+        });
+
+        // then --> verify the output
+        Mockito.verify(employeeRepository, Mockito.never()).save(Mockito.any(Employee.class));
     }
 }
